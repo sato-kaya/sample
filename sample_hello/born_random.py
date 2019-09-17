6ee36bee48ff882ad8e9f24cc34fcd7665888540
sato-kaya <663indk1192@gmail.com>
2019-09-17 14:10:24 +0900

    ファイル書き込み機能追加

--- sample_hello/born_random.py
+++ sample_hello/born_random.py
@@ -18,30 +18,39 @@ b = int(input("何個も作成しますか："))
 # 大文字か小文字が混合か確認
 up_low = int(input('大文字の場合は０、小文字の場合は１、混合で作る場合は２を入力してください：'))
 
-if up_low == 0:
-    for x in range(b):
-        res = ''
-        for y in range(a):
-            ran = random.randint(0, 25)
-            res += double_alphabet[0][ran]
-        print(res, end=" ")
-elif up_low == 1:
-    for x in range(b):
-        res = ''
-        for y in range(a):
-            ran = random.randint(0, 25)
-            res += double_alphabet[1][ran]
-        print(res, end=" ")
-else:
-    for x in range(b):
-        res = ""
-        for y in range(a):
-            # 2次元配列のどっちにするのか決める
-            xy = random.randint(0, 1)
-            if xy == 0:
+
+with open('all_random.txt', encoding="utf-8", mode="w") as f:
+
+    if up_low == 0:
+        for x in range(b):
+            res = ''
+            for y in range(a):
                 ran = random.randint(0, 25)
                 res += double_alphabet[0][ran]
-            else:
+            res += " "
+            print(res, end="")
+            f.write(res)
+    elif up_low == 1:
+        for x in range(b):
+            res = ''
+            for y in range(a):
                 ran = random.randint(0, 25)
                 res += double_alphabet[1][ran]
-        print(res, end=" ")
+            res += " "
+            print(res, end="")
+            f.write(res)
+    else:
+        for x in range(b):
+            res = ""
+            for y in range(a):
+                # 2次元配列のどっちにするのか決める
+                xy = random.randint(0, 1)
+                if xy == 0:
+                    ran = random.randint(0, 25)
+                    res += double_alphabet[0][ran]
+                else:
+                    ran = random.randint(0, 25)
+                    res += double_alphabet[1][ran]
+            res += " "
+            print(res, end="")
+            f.write(res)
